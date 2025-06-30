81f801df8bd2fb11ca29f1ec0a652e30352804dc
Bonolo Mphahlele <bonolomphahlele@gmail.com>
2025-06-30 18:25:52 +0200

    Add docstrings to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import Candle, RoomSpray
+
 # Register your models here.
 
 @admin.register(Candle)
 class CandleAdmin(admin.ModelAdmin):
+    """
+    Admin view configuration for Candle model.
+    """
     list_display = ('name', 'price')
+
 
 @admin.register(RoomSpray)
 class RoomSprayAdmin(admin.ModelAdmin):
+    """
+    Admin view configuration for RoomSpray model.
+    """
     list_display = ('name', 'price')
