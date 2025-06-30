e737ccb7a6cb0687a734467f88fec0881b3afc97
Bonolo Mphahlele <bonolomphahlele@gmail.com>
2025-06-30 18:26:15 +0200

    Add dummy test with docstring to tests.py

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class BasicTestCase(TestCase):
+    """
+    A basic placeholder test case.
+    """
+    def test_example(self):
+        """
+        Dummy test to check the test runner.
+        """
+        self.assertEqual(1 + 1, 2)
