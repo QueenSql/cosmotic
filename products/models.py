bd8e7581adcb43649cc8fe95e31954f38cdba2ce
Bonolo Mphahlele <bonolomphahlele@gmail.com>
2025-06-30 18:25:29 +0200

    Add docstrings to models.py

--- products/models.py
+++ products/models.py
@@ -3,19 +3,32 @@ from django.db import models
 # Create your models here.
 
 class Candle(models.Model):
+    """
+    Represents a candle product with name, price, image, and description.
+    """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='candles/')
     description = models.TextField()
 
     def __str__(self):
+        """
+        Returns the name of the candle for display purposes.
+        """
         return self.name
 
+
 class RoomSpray(models.Model):
+    """
+    Represents a room spray product with name, price, image, and description.
+    """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='room_sprays/')
     description = models.TextField()
 
     def __str__(self):
+        """
+        Returns the name of the room spray for display purposes.
+        """
         return self.name
